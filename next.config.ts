d9d11f345881a1700759f05a10568a33f0430db4
Nikhil-Rao-789 <cnikhilrao2006@gmail.com>
2026-04-04 21:05:10 +0530

    Anilist integration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "s4.anilist.co",
-      },
-      {
-        protocol: "https",
-        hostname: "img.anili.st",
+        protocol: 'https',
+        hostname: 's4.anilist.co',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
