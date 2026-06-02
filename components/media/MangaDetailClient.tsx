c5535525151e6e41b8e14b923830728fb261347b
Shlok Deshmukh <shlokdeshmukh24bcs0111@iiitkottayam.ac.in>
2026-06-02 21:27:29 +0530

    Update MangaDetailClient.tsx
    Signed-off-by: Shlok Deshmukh <shlokdeshmukh24bcs0111@iiitkottayam.ac.in>

--- components/media/MangaDetailClient.tsx
+++ components/media/MangaDetailClient.tsx
@@ -69,12 +69,11 @@ export function MangaDetailClient({ id }: MangaDetailClientProps) {
   };
 
   return (
-    return (
   <div
     className="min-h-screen bg-cover bg-center bg-fixed"
     style={{
       backgroundImage:
-        "linear-gradient(rgba(0,0,0,.85), rgba(0,0,0,.92)), url('/bg1.jpg')",
+        "linear-gradient(rgba(0,0,0,.85), rgba(0,0,0,.92)), url('/bg2.jpg')",
     }}
   >
       {bannerImage && (
